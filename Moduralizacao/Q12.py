d1ac13d6ce40c7bab595d2b88ed795a0814dd303
MarcosCastelo <marcosvitorcastelo@gmail.com>
2017-08-15 17:11:56 -0300

    Q12
    media ponderada

---
+++ Moduralizacao/Q12.py
@@ -0,0 +1,9 @@
+a = float(input("Digite um numero: "))
+b = float(input("Digite um numero: "))
+c = float(input("Digite um numero: "))
+d = float(input("Digite um numero: "))
+
+def media(a,b,c,d):
+    return (a*1 + b*2 + c*3 + d*4) / 10
+
+print("A media ponderada é: ",media(a,b,c,d))
