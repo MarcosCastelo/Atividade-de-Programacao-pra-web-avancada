699e1088f59ae5162c03638398fbacf8f8f3859c
MarcosCastelo <marcosvitorcastelo@gmail.com>
2017-08-15 14:54:05 -0300

    Q11
    operacao soma e multiplicacao

---
+++ Sequencia/Q11.py
@@ -0,0 +1,5 @@
+a = float(input("Digite o primeiro numero: "))
+b = float(input("Digite o segundo numero: "))
+c = float(input("Digite o terceiro numero: "))
+
+print("A soma do numero 1 com o numero 2, multiplicado pela soma do numero 2 pelo 3 eh: ", (a + b)*(b + c))
