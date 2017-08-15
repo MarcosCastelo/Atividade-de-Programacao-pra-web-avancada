f46a2346bd8ca5b748b0cb0a10de0e40b42929cd
MarcosCastelo <marcosvitorcastelo@gmail.com>
2017-08-15 15:09:42 -0300

    Q14
    inverter dois numeros

---
+++ Sequencia/Q14.py
@@ -0,0 +1,8 @@
+a = int(input("Digite o primeiro numero: "))
+b = int(input("Digite o segundo numero: "))
+
+a = a + b
+b = a - b
+a = a - b
+
+print("Os valores invertidos do primeiro e segundo numero é: ", a,"e",b)
