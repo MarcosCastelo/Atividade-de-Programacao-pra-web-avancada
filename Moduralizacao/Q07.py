480af0a8317f0980c10ceb0c7c5d1a1e78dc5487
MarcosCastelo <marcosvitorcastelo@gmail.com>
2017-08-15 16:34:10 -0300

    Questões
    questoes da lista de modularização

---
+++ Moduralizacao/Q07.py
@@ -0,0 +1,6 @@
+def operacao(a,b):
+    return ((a+b)/(a-b))
+a = int(input("Digite o 1° numero: "))
+b = int(input("Digite o 2° numero: "))
+
+print("A divisão da soma pela subtração dos numeros é: ",operacao(a,b))
